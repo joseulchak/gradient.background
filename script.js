4115e8d7ce5a5ef9e5e9598e650a49223bcad0bf
José Ulchak <joseulchak@gmail.com>
2020-10-07 22:06:16 -0300

    added random colors

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var currCss = document.querySelector(".currCss");
 var body = document.getElementById("bg");
+var randomButton = document.querySelector(".randomButton");
 
 function setBgColor() {
     body.style.background = "linear-gradient(to right, " + color1.value +", " + color2.value + ")";
     currCss.textContent = "linear-gradient(to right, " + color1.value +", " + color2.value + ")";
 }
 
+function randomColor() {
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function setRandomColor() {
+    color1.value = "#"+randomColor();
+    color2.value  = "#"+randomColor();
+    body.style.background = "linear-gradient(to right, " + color1.value +", " + color2.value + ")";
+    currCss.textContent = "linear-gradient(to right, " + color1.value +", " + color2.value + ")";
+}
+
+setBgColor();
+
 color1.addEventListener("input", setBgColor);
 color2.addEventListener("input", setBgColor);
 
+randomButton.addEventListener("click", setRandomColor);
+
+randomButton.addEventListener("mouseenter", () => {
+    randomButton.style.background = "#"+randomColor();
+});
